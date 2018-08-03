28615bfe84c75135a41443ea4e78bab23e9334ac
Dancheek <dancheek22@gmail.com>
2018-08-03 13:48:11 +0400

    basic world generation

--- World.py
+++ World.py
@@ -1,13 +1,17 @@
+from random import randint
+import Cell
+
 class World:
 	def __init__(self, width, height):
 		self.width = width
 		self.height = height
+		self.world_reset()
 
-        def reset(self):
-            self.cells=[]
+	def world_reset(self):
+		self.cells = [[None for y in range(self.height)] for x in range(self.width)]
 
-        def gen(self,max_x,max_y):
-            for(i=0; i<max_x; i++)
-                for(j=0; j<max_y; j++)
-                    #random 0..1000 -> if (res == 0) self.cells[i][j]=new_cell(i,j)
-                    #
+	def gen(self, max_x, max_y):
+		for x in range(max_x):
+			for y in range(max_y):
+				if randint(1, 1000) == 1:
+					self.cells[x][y] = Cell.Cell(x, y)
