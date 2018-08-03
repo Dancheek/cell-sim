0e9d3f4111174b7cb5e04e785cf9609c7410910b
Dancheek <dancheek22@gmail.com>
2018-08-03 20:30:35 +0400

    massive update

--- Cell.py
+++ Cell.py
@@ -1,7 +1,46 @@
+import World
+from random import randint 
+
+GENOME_LENGHT = 64
+ENERGY_LIMIT = 200
+
 class Cell:
-	def __init__(self, x, y, color):
+	def __init__(self, x, y, color, parent_genome=None):
 		self.x = x
 		self.y = y
+		self.energy = 100
+		self.genome_pointer = 0
 		self.color = color
-	def do_step():
-		pass
+
+		if parent_genome is not None:
+			self.genome = [23 for i in range(GENOME_LENGHT)]
+		else:
+			self.genome = parent_genome
+			if randint(1, 4) == 4:
+				self.genome[randint(0, 63)] = randint(0, 63)
+
+	def get_genome_content(self, index):
+		if index >= GENOME_LENGHT:
+			return self.genome[index - GENOME_LENGHT]
+		return self.genome[index]
+
+	def do_step(self):
+		current_genome_content = self.get_genome_content(self.genome_pointer)
+		if current_genome_content == 23:
+			self.photosynthesis(self)
+		else:
+			self.genome_pointer += current_genome_content
+
+		if self.genome_pointer >= GENOME_LENGHT:
+			self.genome_pointer -= GENOME_LENGHT
+
+		if self.energy >= ENERGY_LIMIT:
+			self.create_child()
+
+	def create_child(self):
+		self.energy -= 100
+
+# --------- Genome commands ----------
+
+	def photosynthesis(self):
+		self.energy += World.get_light_energy(self.x, self.y)
--- World.py
+++ World.py
@@ -1,6 +1,9 @@
 from random import randint
 import Cell
 
+SOLAR_POWER = 32
+SOLAR_FADING = 2
+
 class World:
 	def __init__(self, width, height):
 		self.width = width
@@ -13,7 +16,10 @@ class World:
 	def gen(self):
 		for x in range(self.width):
 			for y in range(self.height):
-				if randint(1, 1000) == 1:
+				if randint(1, 1000) == 1000:
 					print(x, y)
 					color = (((x + 1) * (y + 1) - 1) % 255, 255, 0)
 					self.cells[x][y] = Cell.Cell(x, y, color)
+
+	def get_light_energy(self, x, y):
+		return (SOLAR_POWER - y) / SOLAR_FADING
