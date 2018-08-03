b82e1bbfcc5ce5a7b05770836ca866970f2ca42a
lexasub <lexakopp2212@gmail.com>
2018-08-03 12:53:02 +0000

    изменено:      World.py

--- World.py
+++ World.py
@@ -2,3 +2,12 @@ class World:
 	def __init__(self, width, height):
 		self.width = width
 		self.height = height
+
+        def reset(self):
+            self.cells=NULL
+
+        def gen(self,max_x,max_y):
+            for(i=0; i<max_x; i++)
+                for(j=0; j<max_y; j++)
+                    #random 0..1000 -> if (res == 0) self.cells[i][j]=new_cell(i,j)
+                    #
